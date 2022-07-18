6b93da52c9420f8d9c8c2115a0d6266f0de58d13
skornous <skornous@users.noreply.github.com>
2022-07-18 21:20:16 +0200

    refactor: fix setPage ctx type

--- src/components/App/App.contexts.ts
+++ src/components/App/App.contexts.ts
@@ -1,4 +1,4 @@
 import { createContext } from "react";
 
-export type PokeListCtxType = { page: number; setPage: (_: number) => void };
+export type PokeListCtxType = { page: number; setPage: (newPage: number) => void };
 export const PokeListCtx = createContext<PokeListCtxType>({ page: 0, setPage: (_: number) => {} });
